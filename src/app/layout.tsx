045cc6cc454e79446dec422a0b3eddf315b48436
lucaslima44 <lucaslima2011h@gmail.com>
2024-08-15 14:34:12 -0300

    Revert "fix: favicon"
    This reverts commit 6e26912b3ac790f1765563baf5115b748e509a7f.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   title: "Imobiliária",
   description: "A imobiliária de sua confiança está aqui.",
   icons: {
-    icon: "/icon.png",
+    icon: "/favicon.ico",
   },
 };
 
