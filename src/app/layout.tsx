6e26912b3ac790f1765563baf5115b748e509a7f
lucaslima44 <lucaslima2011h@gmail.com>
2024-08-15 14:32:09 -0300

    fix: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   title: "Imobiliária",
   description: "A imobiliária de sua confiança está aqui.",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/icon.png",
   },
 };
 
